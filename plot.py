393982662c6408971d47e8f980819feb6ec7c488
Nithish Reddy <nithishreddyb2@gmail.com>
2026-04-26 22:19:52 +0530

    updated file

--- plot.py
+++ plot.py
@@ -5,56 +5,32 @@ from scipy.signal import find_peaks
 
 DATA_PATH = "data/processed/processed_ftir_26thApril.csv"
 
-ROW_INDEX = 0
-PEAK_PROMINENCE = 0.05 
+ROW_INDICES = [0,923,1042,1628,2145,2255,2913]
+PEAK_PROMINENCE = 0.05
 
-f = pd.read_csv(DATA_PATH)
-
-row = df.iloc[ROW_INDEX]
-
-polymer = row["Polymer"]
-sample_id = row["Sample_ID"]
+df = pd.read_csv(DATA_PATH)
 
 feature_cols = df.columns[2:]
-
 wavenumbers = np.array([int(col.split("_")[1]) for col in feature_cols])
-intensity = row[feature_cols].values.astype(float)
-
-inverted_signal = -intensity
-
-peaks, properties = find_peaks(
-    inverted_signal,
-    prominence=PEAK_PROMINENCE
-)
 
 plt.figure(figsize=(12, 6))
 
-plt.plot(wavenumbers, intensity, label="FTIR Spectrum")
+for idx in ROW_INDICES:
+    row = df.iloc[idx]
+    intensity = row[feature_cols].values.astype(float)
 
-plt.scatter(
-    wavenumbers[peaks],
-    intensity[peaks],
-    color="red",
-    label="Detected Peaks",
-    zorder=5
-)
+    inverted_signal = -intensity
+    peaks, _ = find_peaks(inverted_signal, prominence=PEAK_PROMINENCE)
 
-for p in peaks:
-    plt.text(
-        wavenumbers[p],
-        intensity[p],
-        str(wavenumbers[p]),
-        fontsize=8,
-        rotation=90,
-        verticalalignment='bottom'
-    )
+    label = f"{row['Polymer']} ({row['Sample_ID']})"
+
+    plt.plot(wavenumbers, intensity, label=label)
+    plt.scatter(wavenumbers[peaks], intensity[peaks], s=20)
 
 plt.gca().invert_xaxis()
-
 plt.xlabel("Wavenumber (cm⁻¹)")
 plt.ylabel("Transmittance (%)")
-plt.title(f"FTIR Spectrum | Polymer: {polymer} | Sample: {sample_id}")
-
+plt.title("FTIR Spectra")
 plt.legend()
 plt.grid(True)
 
